3f8d89553ca258dd0fa62becc5d05043e91cdd81
officialnezt <ernestprabowo@gmail.com>
2022-06-05 23:24:12 +0700

    login

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="cus-index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+  </head>
+  <body>
+  
+      <nav class="navbar navbar-expand-lg bg-light p-0 m-0">
+        <div class="container-fluid" style="background-color: rgba(28, 57, 101, 1);"> 
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="">
+            <div class="logo"><img src="asset/build-logo.png" alt="build"></div>
+          </div>
+          <div class="collapse navbar-collapse justify-content-center"  id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">AboutUs</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">ContactUs</a>
+              </li>
+            </ul>
+          </div>
+          <div class="d-flex">
+            <div class="sign-in">
+              <a href="#" class="  btn btn-outline-light my-3 mx-3 fs-4">Sign in</a>
+            </div>
+            <div class="get-started">
+              <a href="#" class="   btn btn-outline-light my-3 mx-3 fs-4">Get Started</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="py-2" style="background-color: yellow;"></div> 
+      <div class="container-fluid d-grid" style="background-color: rgba(28, 57, 101, 1);">
+        <div class="row">
+          <div class="col-md-5  p-5 justify-content-center text-light" style="margin-left: 10vw">
+            <div class="title-1" style="font-size: 10em;">BUILD</div>
+            <div class="title-2 " style="font-size: 5em;">With Just</div>
+            <div class="title-3" style="font-size: 4em;">one <span class="title-4  pl-0 pr-2 px-2 text-dark" style="background-color:yellow; font-size:2em">Click</span></div>
+          </div>
+
+          <div class="col-md-5  ms-5 p-5 justify-content-center">
+              <img src="asset/jumbo2.png" alt="ernest">
+          </div>
+        </div>
+      <div class="cus-container mx-2" style="background-color: rgba(28, 57, 101, 1);">
+        <div class="card ">
+          <img class="img-1" src="./asset/card/1.png" alt="1">
+          <div class="intro">
+            <h1>Material</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="img-2" src="./asset/card/1.png" alt="1">
+          <div class="intro">
+            <h1>Material</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="img-3" src="./asset/card/1.png" alt="1">
+          <div class="intro">
+            <h1>Material</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="img-3" src="./asset/card/1.png" alt="1">
+          <div class="intro">
+            <h1>Material</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+          </div>
+        </div>
+      </div>
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+  <footer>
+    <div style="background-color: yellow;" class="text-center mt-2">
+        <h2>BUILD APP</h2>
+    </div>
+  </footer>
+</html>
