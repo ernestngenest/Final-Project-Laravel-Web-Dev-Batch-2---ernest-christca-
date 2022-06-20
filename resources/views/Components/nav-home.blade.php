e95cc915146a2cb83cf5c8a13add7913f8b50946
Octopuzzz <reagentreagent4@gmail.com>
2022-06-20 08:31:34 +0700

    nav

--- resources/views/Components/nav-home.blade.php
+++ resources/views/Components/nav-home.blade.php
@@ -7,22 +7,22 @@
             <div class="">
             <div class="logo"><img src="{{ asset('assets/build-logo.png'); }}" alt="build"></div>
             </div>
+            <div class="collapse navbar-collapse justify-content-center"  id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">AboutUs</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">ContactUs</a>
+                    </li>
+                </ul>
+            </div>
             @auth
 
-                @else
-                <div class="collapse navbar-collapse justify-content-center"  id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">AboutUs</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link active my-3 mx-5 text-light fs-2" aria-current="page" href="#">ContactUs</a>
-                        </li>
-                    </ul>
-                </div>
+            @else
                 <div class="d-flex">
                     <div class="sign-in">
                         <a href="/auth/login" class="  btn btn-outline-light my-3 mx-3 fs-4">Sign in</a>
