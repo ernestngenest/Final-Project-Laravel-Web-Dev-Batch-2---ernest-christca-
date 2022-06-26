86acde5b7f5e3d831f5b4b6d4bf53fece509e97b
ernest christca <ernestprabowo@gmail.com>
2022-06-26 19:58:21 +0700

    dasdas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,16 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $material = DB::table('categories')
+        ->join('materials', 'categories.id', '=', 'materials.category_id')
+        ->select( 'materials.material_name', 'materials.material_price' , 'materials.material_price', 'materials.material_image','materials.material_description',)
+        ->get();
+
+        $worker = DB::table('categories')
+        ->join('constructor_workers', 'categories.id', '=', 'constructor_workers.category_id')
+        ->select('constructor_workers.constructor_worker_name', 'constructor_workers.constructor_worker_image' ,
+        'constructor_workers.constructor_worker_price' , 'constructor_workers.constructor_worker_quantity')
+        ->get();
+
         return view('/dashboard/Components/dashboard', [
             'status' => 'Dashboard',
-            'auth_status' => 1
-        ]);
+            'materials' => $material,
+            'workers' => $worker
+        ])->with('success', 'Data berhasil ditambahkan');
     }
+
     public function __construct()
     {
     }
