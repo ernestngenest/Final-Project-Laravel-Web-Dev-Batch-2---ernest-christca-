65ee08f849d5aff56a19434ff001f4e61bc5d29a
Octopuzzz <reagentreagent4@gmail.com>
2022-06-26 22:29:40 +0700

    Authentication Search

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,15 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
+        if (!Auth::check()) {
+            return redirect('/');
+        }
         $material = DB::table('categories')
             ->join('materials', 'categories.id', '=', 'materials.category_id')
             ->select('materials.material_name', 'materials.material_price', 'materials.material_price', 'materials.material_image', 'materials.material_description',)
@@ -25,7 +29,7 @@ class DashboardController extends Controller
                 'constructor_workers.constructor_worker_quantity'
             )
             ->get();
-        if($request->has('search')){
+        if ($request->has('search')) {
             $search = $request->get('search');
             $material = DB::table('categories')
                 ->join('materials', 'categories.id', '=', 'materials.category_id')
