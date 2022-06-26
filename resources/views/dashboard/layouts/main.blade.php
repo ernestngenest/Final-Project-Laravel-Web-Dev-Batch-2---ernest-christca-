58bdbd58f4674583719f682090b0cbecf7d046a6
ernest christca <ernestprabowo@gmail.com>
2022-06-26 17:58:22 +0700

    dash update

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -15,9 +15,9 @@
 </head>
     <body>
         @include('dashboard.Components.nav-dashboard')
-        <div class="container-fluid p-0">
+
             @yield('content');
-        </div>
+ 
         <x-footer-home></x-footer-home>
         <script src="{{ asset('js/dash.js') }}"> </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
